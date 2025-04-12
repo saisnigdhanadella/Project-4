183014317a504b88f640a2ef1a6ea0c2a831817a
saisnigdhanadella <snadella1@binghamton.edu>
2025-04-11 22:21:12 -0400

    Update SimplfFunction.java

--- simplf/SimplfFunction.java
+++ simplf/SimplfFunction.java
@@ -4,28 +4,30 @@ import java.util.List;
 
 class SimplfFunction implements SimplfCallable {
     private final Stmt.Function declaration;
-    private final Environment closure;
+    private Environment closure;
 
     SimplfFunction(Stmt.Function declaration, Environment closure) {
         this.declaration = declaration;
         this.closure = closure;
     }
 
+    public void setClosure(Environment environment) {
+        this.closure = environment;
+    }
+
     @Override
     public Object call(Interpreter interpreter, List<Object> args) {
-        Environment functionEnv = new Environment(closure);
+        Environment env = new Environment(closure);
         for (int i = 0; i < declaration.params.size(); i++) {
-            Token param = declaration.params.get(i);
-            functionEnv = functionEnv.define(param, param.lexeme, args.get(i));
+            env = env.define(declaration.params.get(i), declaration.params.get(i).lexeme, args.get(i));
         }
 
-        try {
-            interpreter.executeBlock(declaration.body, functionEnv);
-        } catch (Return r) {
-            return r.value;
+        Object result = null;
+        for (Stmt stmt : declaration.body) {
+            result = interpreter.execute(stmt);
         }
 
-        return null;
+        return result;
     }
 
     @Override
